5e5981b1f38e18b878996f5caddcf7526057df25
kolpakovee <eekolpakov@edu.hse.ru>
2022-11-20 00:44:37 +0300

    add comments

--- part1_e_cl.s
+++ part1_e_cl.s
@@ -3,15 +3,15 @@
 	.text
 	.globl	randomValue
 	.type	randomValue, @function
-randomValue:
+randomValue:                                #функция randomValue
 	endbr64
 	push	rbp
 	mov	rbp, rsp
 	mov	edi, 0
-	call	time@PLT
+	call	time@PLT                        #вызов функции time
 	mov	edi, eax
-	call	srand@PLT
-	call	rand@PLT
+	call	srand@PLT                       #вызов функции srand
+	call	rand@PLT                        #вызов функции rand
 	pxor	xmm0, xmm0
 	cvtsi2sd	xmm0, eax
 	movsd	xmm2, QWORD PTR .LC0[rip]
@@ -24,7 +24,7 @@ randomValue:
 	movq	rax, xmm0
 	movq	xmm0, rax
 	pop	rbp
-	ret
+	ret                                     #взоврат значения из функции randomValue
 	.size	randomValue, .-randomValue
 	.section	.rodata
 .LC4:
@@ -44,13 +44,13 @@ randomValue:
 	.text
 	.globl	main
 	.type	main, @function
-main:
+main:                                       #функция main
 	endbr64
 	push	rbp
 	mov	rbp, rsp
 	sub	rsp, 144
-	mov	DWORD PTR -116[rbp], edi
-	mov	QWORD PTR -128[rbp], rsi
+	mov	DWORD PTR -116[rbp], edi            #хранение параметра функции int argc
+	mov	QWORD PTR -128[rbp], rsi            #хранение парметра функции char** argv
 	pxor	xmm0, xmm0
 	movsd	QWORD PTR -8[rbp], xmm0
 	cmp	DWORD PTR -116[rbp], 2
@@ -62,9 +62,9 @@ main:
 	cmp	al, 49
 	jne	.L4
 	mov	eax, 0
-	call	randomValue
+	call	randomValue                     #вызов функции randomValue без передачи параметров
 	movq	rax, xmm0
-	mov	QWORD PTR -104[rbp], rax
+	mov	QWORD PTR -104[rbp], rax            #val := randomValue()
 	jmp	.L5
 .L4:
 	cmp	DWORD PTR -116[rbp], 2
@@ -75,12 +75,12 @@ main:
 	movzx	eax, BYTE PTR [rax]
 	cmp	al, 50
 	jne	.L6
-	lea	rax, -104[rbp]
+	lea	rax, -104[rbp]                      #параметр &val для функции scanf
 	mov	rsi, rax
-	lea	rax, .LC4[rip]
+	lea	rax, .LC4[rip]                      #строка-формат для вызова функциии scanf
 	mov	rdi, rax
 	mov	eax, 0
-	call	__isoc99_scanf@PLT
+	call	__isoc99_scanf@PLT              #вызов функции scanf
 	jmp	.L5
 .L6:
 	cmp	DWORD PTR -116[rbp], 4
@@ -91,36 +91,36 @@ main:
 	movzx	eax, BYTE PTR [rax]
 	cmp	al, 51
 	jne	.L7
-	mov	rax, QWORD PTR -128[rbp]
+	mov	rax, QWORD PTR -128[rbp]            #argv[2] для вызова функции fopen(argv[2], "r")
 	add	rax, 16
 	mov	rax, QWORD PTR [rax]
-	lea	rdx, .LC5[rip]
+	lea	rdx, .LC5[rip]                      #строка-формат "r" для вызова функции fopen(argv[2], "r")
 	mov	rsi, rdx
 	mov	rdi, rax
-	call	fopen@PLT
-	mov	QWORD PTR -32[rbp], rax
-	lea	rdx, -104[rbp]
-	mov	rax, QWORD PTR -32[rbp]
-	lea	rcx, .LC4[rip]
+	call	fopen@PLT                      #вызов функции fopen
+	mov	QWORD PTR -32[rbp], rax            #FILE *input_file := результат функции fopen
+	lea	rdx, -104[rbp]                      #параметр &val для функции fscanf
+	mov	rax, QWORD PTR -32[rbp]             #параметр FILE *input_file для вызова функции fscanf
+	lea	rcx, .LC4[rip]                      #строка-формат "%lf" для ввода переменной типа double
 	mov	rsi, rcx
 	mov	rdi, rax
 	mov	eax, 0
-	call	__isoc99_fscanf@PLT
+	call	__isoc99_fscanf@PLT             #вызов функции fscanf
 	jmp	.L5
 .L7:
-	lea	rax, .LC6[rip]
+	lea	rax, .LC6[rip]                      #строка-формат для вызова функции printf
 	mov	rdi, rax
 	mov	eax, 0
-	call	printf@PLT
+	call	printf@PLT                      #вызов функции printf
 	mov	eax, 0
 	jmp	.L17
 .L5:
 	pxor	xmm0, xmm0
 	movsd	QWORD PTR -16[rbp], xmm0
-	lea	rax, -80[rbp]
+	lea	rax, -80[rbp]                       #переменная &start для вызова функции clock_gettime
 	mov	rsi, rax
 	mov	edi, 1
-	call	clock_gettime@PLT
+	call	clock_gettime@PLT               #вызов функции clock_gettime
 	mov	DWORD PTR -20[rbp], 0
 	jmp	.L9
 .L14:
@@ -175,10 +175,10 @@ main:
 .L9:
 	cmp	DWORD PTR -20[rbp], 999
 	jle	.L14
-	lea	rax, -96[rbp]
+	lea	rax, -96[rbp]                           #переменная &end для вызова функции clock_gettime
 	mov	rsi, rax
 	mov	edi, 1
-	call	clock_gettime@PLT
+	call	clock_gettime@PLT                   #вызов функции clock_gettime
 	mov	rax, QWORD PTR -96[rbp]
 	pxor	xmm1, xmm1
 	cvtsi2sd	xmm1, rax
@@ -202,22 +202,22 @@ main:
 	mov	QWORD PTR -40[rbp], rax
 	cmp	DWORD PTR -116[rbp], 4
 	jne	.L15
-	mov	rax, QWORD PTR -128[rbp]
+	mov	rax, QWORD PTR -128[rbp]            #rax := argv[3] для вызова функции fopen
 	add	rax, 24
 	mov	rax, QWORD PTR [rax]
-	lea	rdx, .LC12[rip]
+	lea	rdx, .LC12[rip]                     #получение строка-формата для вызова функции fopen
 	mov	rsi, rdx
 	mov	rdi, rax
-	call	fopen@PLT
-	mov	QWORD PTR -48[rbp], rax
-	mov	rdx, QWORD PTR -8[rbp]
-	mov	rax, QWORD PTR -48[rbp]
+	call	fopen@PLT                       #вызов функции fopen
+	mov	QWORD PTR -48[rbp], rax             #FILE *output_file := результат вызова fopen
+	mov	rdx, QWORD PTR -8[rbp]              #rdx := result для вызова функции fprintf
+	mov	rax, QWORD PTR -48[rbp]             #rax := output_file для вызова функции fprintf
 	movq	xmm0, rdx
-	lea	rdx, .LC13[rip]
+	lea	rdx, .LC13[rip]                     #получение строки-формата для вызова функции fprintf
 	mov	rsi, rdx
 	mov	rdi, rax
 	mov	eax, 1
-	call	fprintf@PLT
+	call	fprintf@PLT                     #вызов функции fprintf
 	movsd	xmm0, QWORD PTR -104[rbp]
 	movq	xmm1, QWORD PTR .LC14[rip]
 	xorpd	xmm0, xmm1
@@ -242,28 +242,28 @@ main:
 	movsd	xmm0, QWORD PTR .LC2[rip]
 	mulsd	xmm1, xmm0
 	movq	rdx, xmm1
-	mov	rax, QWORD PTR -48[rbp]
+	mov	rax, QWORD PTR -48[rbp]             #rax := output_file для вызова функции fprintf
 	movq	xmm0, rdx
-	lea	rdx, .LC15[rip]
+	lea	rdx, .LC15[rip]                     #получение строки-формата для вызова функции fprintf
 	mov	rsi, rdx
 	mov	rdi, rax
 	mov	eax, 1
-	call	fprintf@PLT
-	mov	rdx, QWORD PTR -40[rbp]
-	mov	rax, QWORD PTR -48[rbp]
+	call	fprintf@PLT                     #вызов функции fprintf
+	mov	rdx, QWORD PTR -40[rbp]             #result_time для функции fprintf
+	mov	rax, QWORD PTR -48[rbp]             #rax := output_file для вызова функции fprintf
 	lea	rcx, .LC16[rip]
 	mov	rsi, rcx
 	mov	rdi, rax
 	mov	eax, 0
-	call	fprintf@PLT
+	call	fprintf@PLT                     #вызов функции fprintf
 	jmp	.L16
 .L15:
-	mov	rax, QWORD PTR -8[rbp]
+	mov	rax, QWORD PTR -8[rbp]              #result для вызова функции printf
 	movq	xmm0, rax
-	lea	rax, .LC13[rip]
+	lea	rax, .LC13[rip]                     #получение строки-формата для вызова функции printf
 	mov	rdi, rax
 	mov	eax, 1
-	call	printf@PLT
+	call	printf@PLT                      #вызов функции printf
 	movsd	xmm0, QWORD PTR -104[rbp]
 	movq	xmm1, QWORD PTR .LC14[rip]
 	xorpd	xmm0, xmm1
@@ -289,21 +289,21 @@ main:
 	mulsd	xmm1, xmm0
 	movq	rax, xmm1
 	movq	xmm0, rax
-	lea	rax, .LC15[rip]
+	lea	rax, .LC15[rip]                     #получение строки-формата для вызова функции printf
 	mov	rdi, rax
 	mov	eax, 1
-	call	printf@PLT
-	mov	rax, QWORD PTR -40[rbp]
+	call	printf@PLT                      #вызов функции printf
+	mov	rax, QWORD PTR -40[rbp]             #result_time для вызова функции printf
 	mov	rsi, rax
-	lea	rax, .LC16[rip]
+	lea	rax, .LC16[rip]                     #получение строки-формата для вызова функции printf
 	mov	rdi, rax
 	mov	eax, 0
-	call	printf@PLT
+	call	printf@PLT                      #вызов функции printf
 .L16:
 	mov	eax, 0
 .L17:
 	leave
-	ret
+	ret                                     #возврат функции main
 	.size	main, .-main
 	.section	.rodata
 	.align 8
--- part2_e_cl.s
+++ part2_e_cl.s
@@ -7,11 +7,11 @@ factorial:
 	endbr64
 	push	rbp
 	mov	rbp, rsp
-	movsd	QWORD PTR -24[rbp], xmm0
+	movsd	QWORD PTR -24[rbp], xmm0        #параметр функции double x
 	movsd	xmm0, QWORD PTR .LC0[rip]
 	movsd	QWORD PTR -8[rbp], xmm0
 	jmp	.L2
-.L3:
+.L3:                                        #тело цикла
 	movsd	xmm0, QWORD PTR -8[rbp]
 	mulsd	xmm0, QWORD PTR -24[rbp]
 	movsd	QWORD PTR -8[rbp], xmm0
@@ -22,13 +22,13 @@ factorial:
 .L2:
 	movsd	xmm0, QWORD PTR -24[rbp]
 	pxor	xmm1, xmm1
-	comisd	xmm0, xmm1
+	comisd	xmm0, xmm1                      #проверка в цикле
 	ja	.L3
-	movsd	xmm0, QWORD PTR -8[rbp]
-	movq	rax, xmm0
+	movsd	xmm0, QWORD PTR -8[rbp]         #xmn0 := i
+	movq	rax, xmm0                       #rax := xmn0
 	movq	xmm0, rax
 	pop	rbp
-	ret
+	ret                                     #возврат функцией значения i
 	.size	factorial, .-factorial
 	.section	.rodata
 	.align 8
